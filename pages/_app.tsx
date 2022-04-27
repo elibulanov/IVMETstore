bd8f6ac31bd91ab09ac2ecfd43c5752c98e95972
eli <elibulanov@gmail.com>
2022-04-27 15:42:51 -0700

    plausible wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return    <PlausibleProvider domain="ivmet-store.vercel.app">
+  <Component {...pageProps} />
+</PlausibleProvider>
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import Document,{ Html, Head, Main, NextScript } from 'next/document'
 export default class CustomDocument extends Document {
     render() {
         return <Html>
-            <Head>
-            <script defer data-domain="ivmet-store.vercel.app" src="https://plausible.io/js/plausible.js"></script>
-            </Head>
+            <Head></Head>
             <body>
                 <Main />
             </body>
