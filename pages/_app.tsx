17b64e1e9960078d8589edca60b4cef38784926a
eli <elibulanov@gmail.com>
2022-04-27 11:25:11 -0700

    plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+       
+        <script defer data-domain="ivmet-store.vercel.app" src="https://plausible.io/js/plausible.js"></script>
+
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
