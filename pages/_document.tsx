67a2c00c5514037a3892c0919bb96a11c4f86cde
eb-web <elibulanov@gmail.com>
2022-04-29 15:38:15 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class CustomDocument extends Document {
             <Head>
             <script defer data-domain="ivmet.com" src="https://plausible.io/js/plausible.js"></script>
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8358664070447163"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
             </Head>
             <body>
                 <Main />
